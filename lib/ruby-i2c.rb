24b0e70dabcc3b24dc56ae19e244132be25facd5
Dean Brundage <github@deanandadie.net>
2016-11-18 20:42:29 -0600

    First!

---
+++ lib/ruby-i2c.rb
@@ -0,0 +1,6 @@
+require 'ruby-i2c/version'
+class RubyI2C
+
+  autoload :Adapter, File.join('ruby-i2c','adapter')
+
+end
---
+++ lib/ruby-i2c/version.rb
@@ -0,0 +1,4 @@
+class RubyI2C
+  VERSION = "0.1.0"
+  def self.version; VERSION; end
+end
